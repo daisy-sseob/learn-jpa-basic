abe9ef03d4a7e4ca55bec2faae2d5222acb957f8
hyun-sseob <orolsyeo@gmail.com>
2021-12-16 21:28:34 +0900

    jpa 간단하게 insert해보기. 🧑🏻‍💻

--- src/main/java/me/sseob/Main.java
+++ src/main/java/me/sseob/Main.java
@@ -2,17 +2,26 @@ package me.sseob;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class Main {
 
 	public static void main(String[] args) {
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("sseob");
+		EntityManager entityManager = entityManagerFactory.createEntityManager(); // entity manager 생성하여 얻음
 
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		
+		Member member = new Member();
+		member.setId(1L);
+		member.setName("sseob");
+		entityManager.persist(member);
+		
+		transaction.commit();
 		
 		entityManager.close();
-		
 		entityManagerFactory.close();
 	}
 }
