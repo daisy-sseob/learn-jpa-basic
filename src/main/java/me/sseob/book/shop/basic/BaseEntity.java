3fcd105a5a564b378900857c50f31c517f6f6871
hyun-sseob <orolsyeo@gmail.com>
2022-01-04 20:27:07 +0900

    Add comment

--- src/main/java/me/sseob/book/shop/basic/BaseEntity.java
+++ src/main/java/me/sseob/book/shop/basic/BaseEntity.java
@@ -4,8 +4,12 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 
+/*
+	MappedSuperClass로는 아무리 어떠한 클래스의 부모타입이더라도 EntityManager를 통해 조회, 검색이 불가능하다.
+	어차피 JPA를 통해 Table로 매핑하여 사용할게 아니기 때문에 추상 클래스로 만들어서 사용한다.
+ */
 @MappedSuperclass // mapping 정보만 받는 super class임을 선언한다.
-public class BaseEntity {
+public abstract class BaseEntity {
 
 	private String createdBy;
 	private LocalDateTime createdDate;
