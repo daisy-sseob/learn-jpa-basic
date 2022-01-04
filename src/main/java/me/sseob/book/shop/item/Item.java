a50b5763533193be7d291ab74297d56634977d0b
hyun-sseob <orolsyeo@gmail.com>
2022-01-04 20:12:56 +0900

    add comment 👀

--- src/main/java/me/sseob/book/shop/item/Item.java
+++ src/main/java/me/sseob/book/shop/item/Item.java
@@ -7,8 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
-	Item Entity를 추상 클래스로 만들 경우 테이블이 생성되지 않는다.
+	TABLE_PER_CLASS 전략: Item Class를 추상 클래스로 만들 경우 테이블이 생성되지 않는다.
 	하지만 상속관계를 갖는 하위 Entity의 테이블들은 생성된다.
+	하지만 Database 설계의 관점에서 item_id column으로 조회를 할 때에 3가지 테이블 모두 조회해야한다는 단점이 있음.
  */
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED) // 객체 상속타입을 토대로 Table 상속 타입을 정할 수 있다.
