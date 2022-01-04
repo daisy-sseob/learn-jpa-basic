ce454e4d8e86152ecc7856a61fc4d95010d2fd7c
hyun-sseob <orolsyeo@gmail.com>
2022-01-04 19:59:03 +0900

    add comment 👀

--- src/main/java/me/sseob/book/shop/item/Item.java
+++ src/main/java/me/sseob/book/shop/item/Item.java
@@ -6,10 +6,14 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+	Item Entity를 추상 클래스로 만들 경우 테이블이 생성되지 않는다.
+	하지만 상속관계를 갖는 하위 Entity의 테이블들은 생성된다.
+ */
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED) // 객체 상속타입을 토대로 Table 상속 타입을 정할 수 있다.
 @DiscriminatorColumn // Item Table에 DTYPE컬럼을 추가해 하위 테이블의 타입을 구분짓는다.
-public class Item {
+public class Item { 
 
 	@Id
 	@GeneratedValue
