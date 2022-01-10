bbf4b2282ec6aca2bc131d64f19adf474f88f8e5
hyun-sseob <orolsyeo@gmail.com>
2022-01-10 21:02:05 +0900

    N+1 문제 확인

--- src/main/java/me/sseob/book/shop/basic/BasicMain.java
+++ src/main/java/me/sseob/book/shop/basic/BasicMain.java
@@ -33,7 +33,8 @@ public class BasicMain {
 			Member member1 = entityManager.find(Member.class, member.getId());
 			System.out.println("member1 = " + member1.getName()); // Lock, Team을 지연로딩으로 설정후 조회하지 않으면 Member만 조회하는 Select query실행.
 			System.out.println("member1.getTeam() = " + member1.getTeam().getClass()); // team hibernate proxy객체가 출력된다. 즉 지연로딩으로 설정하여 proxy객체를 참조하게 만든다. proxy객체 출력후 select query가 실행되는걸 볼 수 있음.
-			System.out.println("member1.getTeam() = " + member1.getTeam().getName()); // 지연 로딩에 의해 나중에 Team이 join된다.
+			System.out.println("member1.getTeam() = " + member1.getTeam().getName()); // 지연 로딩에 의해 나중에 Team이 join된다. 실제 사용될 때 proxy instance 초기화.
+			// 반대로 FetchType.EAGER 즉시로딩을 설정하게 되면 애초에 member, team을 join하여 select한다. 프록시 객체를 사용하지 않아도 된다.
 			
 			transaction.commit();
 		} catch (Exception e){
