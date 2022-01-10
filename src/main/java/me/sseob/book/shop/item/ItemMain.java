99ead69b13d0e0015e87af0fc4798c39358f2e63
hyun-sseob <orolsyeo@gmail.com>
2022-01-10 20:29:08 +0900

    proxy 객체의 전반적인 구조 확인

--- src/main/java/me/sseob/book/shop/item/ItemMain.java
+++ src/main/java/me/sseob/book/shop/item/ItemMain.java
@@ -1,5 +1,7 @@
 package me.sseob.book.shop.item;
 
+import org.hibernate.Hibernate;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -31,9 +33,11 @@ public class ItemMain {
 				하지만 아래 print하는 코드가 있다면 select query를 뒤늦게 실행한다.
 			 */
 			Movie reference = entityManager.getReference(Movie.class, movie.getId());
+			Hibernate.initialize(reference); // proxy 객체 강제 초기화. jpa표준은 아님.
 			System.out.println("movie1.getName() = " + reference.getClass()); // hibernate proxy class가 print 된다. (가짜 entity 객체) 
 			System.out.println("movie1.getName() = " + reference.getName()); // 첫 1회 getName 호출시 영속성 컨텍스트에 객체 초기화를 요청하게 되고 그 과정에서 select query가 발생한다.
 			System.out.println("movie1.getDirector() = " + reference.getDirector()); // select query발생하지 않고 초기화된 프록시 객체 그대로 사용.
+			System.out.println("isLoaded = " + entityManagerFactory.getPersistenceUnitUtil().isLoaded(reference)); // 프록시 instance의 초기화 여부 확인
 
 //			Movie movie1 = entityManager.find(Movie.class, movie.getId());
 //			System.out.println("movie1.getName() = " + movie1.getName());
